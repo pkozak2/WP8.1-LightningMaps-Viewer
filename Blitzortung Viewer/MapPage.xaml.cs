05626995015eb6d95853f7136137d948c079087e
pkozak2 <piotr.k1992@gmail.com>
2016-02-24 21:10:55 +0100

    first

---
+++ Blitzortung Viewer/MapPage.xaml.cs
@@ -0,0 +1,278 @@
+﻿using Blitzortung_Viewer.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Blitzortung_Viewer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MapPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public MapPage()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+
+            
+
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+
+            //address.Text = "http://m.lightningmaps.org/realtime";
+            //NavigateWebview("http://www.microsoft.com");
+            String adres = "http://m.lightningmaps.org/realtime";
+            // String adres = "http://google.pl";
+
+            NavigateWebview(adres);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This is the click handler for the 'Navigation' button.  You would replace this with your own handler
+        /// if you have a button or buttons on this page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void goButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!pageIsLoading)
+            {
+                //NavigateWebview(address.Text);
+            }
+            else
+            {
+                webView.Stop();
+                pageIsLoading = false;
+            }
+        }
+
+        /// <summary>
+        /// This handles the enter key in the url address box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void address_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                //NavigateWebview(address.Text);
+            }
+        }
+
+        /// <summary>
+        /// Helper to perform the navigation in webview
+        /// </summary>
+        /// <param name="url"></param>
+        private void NavigateWebview(string url)
+        {
+
+            try
+            {
+                Uri targetUri = new Uri(url);
+                webView.Navigate(targetUri);
+            }
+            catch (FormatException myE)
+            {
+                // Bad address
+                webView.NavigateToString(String.Format("<h1>Address is invalid, try again.</h1>"));
+            }
+        }
+
+        /// <summary>
+        /// Property to control the "Go" button text, forward/backward buttons and progress ring
+        /// </summary>
+        private bool _pageIsLoading;
+        bool pageIsLoading
+        {
+            get { return _pageIsLoading; }
+            set
+            {
+                _pageIsLoading = value;
+                // goButton.Content = (value ? "Stop" : "Go");
+                progressRing1.Visibility = (value ? Visibility.Visible : Visibility.Collapsed);
+
+                if (!value)
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event to indicate webview is starting a navigation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            string url = "";
+            try { url = args.Uri.ToString(); }
+            finally
+            {
+                // address.Text = url;
+
+                pageIsLoading = true;
+            }
+        }
+
+        /// <summary>
+        /// Event is fired by webview when the content is not a webpage, such as a file download
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void webView1_UnviewableContentIdentified(WebView sender, WebViewUnviewableContentIdentifiedEventArgs args)
+        {
+
+            // We turn around and hand the Uri to the system launcher to launch the default handler for it
+            await Windows.System.Launcher.LaunchUriAsync(args.Uri);
+            pageIsLoading = false;
+        }
+
+        /// <summary>
+        /// Event to indicate webview has resolved the uri, and that it is loading html content
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_ContentLoading(WebView sender, WebViewContentLoadingEventArgs args)
+        {
+            string url = (args.Uri != null) ? args.Uri.ToString() : "<null>";
+        }
+
+        /// <summary>
+        /// Event to indicate that the content is fully loaded in the webview. If you need to invoke script, it is best to wait for this event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
+        {
+            string url = (args.Uri != null) ? args.Uri.ToString() : "<null>";
+
+        }
+
+        /// <summary>
+        /// Event to indicate webview has completed the navigation, either with success or failure.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            pageIsLoading = false;
+            if (args.IsSuccess)
+            {
+                string url = (args.Uri != null) ? args.Uri.ToString() : "<null>";
+
+            }
+            else
+            {
+                string url = "";
+                try { url = args.Uri.ToString(); }
+                finally
+                {
+                    //address.Text = url;
+
+                }
+            }
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+
+            webView.Refresh();
+        }
+
+        
+
+    }
+}
---
+++ Blitzortung Viewer/PivotPage.xaml.cs
@@ -0,0 +1,205 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Documents;
+using System;
+
+// The Pivot Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
+
+namespace Blitzortung_Viewer
+{
+    public sealed partial class PivotPage : Page
+    {
+
+        public PivotPage()
+        {
+            this.InitializeComponent();
+
+            //webView.NavigationStarting += webView_NavigationStarting;
+            //webView.ContentLoading += webView_ContentLoading;
+            //webView.DOMContentLoaded += webView_DOMContentLoaded;
+            //webView.UnviewableContentIdentified += webView_UnviewableContentIdentified;
+            //webView.NavigationCompleted += webView_NavigationCompleted;
+
+        }
+
+
+
+        /// <summary>
+        /// Invoked when this xaml page is about to be displayed in a Frame.
+        /// Note: This event is not related to the webview navigation.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //address.Text = "http://m.lightningmaps.org/realtime";
+            //NavigateWebview("http://www.microsoft.com");
+            String adres = "http://m.lightningmaps.org/realtime";
+           // String adres = "http://google.pl";
+           
+            NavigateWebview(adres);
+        }
+
+        /// <summary>
+        /// This is the click handler for the 'Navigation' button.  You would replace this with your own handler
+        /// if you have a button or buttons on this page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void goButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!pageIsLoading)
+            {
+                //NavigateWebview(address.Text);
+            }
+            else
+            {
+                webView.Stop();
+                pageIsLoading = false;
+            }
+        }
+
+        /// <summary>
+        /// This handles the enter key in the url address box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void address_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                //NavigateWebview(address.Text);
+            }
+        }
+
+        /// <summary>
+        /// Helper to perform the navigation in webview
+        /// </summary>
+        /// <param name="url"></param>
+        private void NavigateWebview(string url)
+        {
+           
+            try
+            {
+                Uri targetUri = new Uri(url);
+                webView.Navigate(targetUri);
+            }
+            catch (FormatException myE)
+            {
+                // Bad address
+                webView.NavigateToString(String.Format("<h1>Address is invalid, try again.</h1>"));
+            }
+        }
+
+        /// <summary>
+        /// Property to control the "Go" button text, forward/backward buttons and progress ring
+        /// </summary>
+        private bool _pageIsLoading;
+        bool pageIsLoading
+        {
+            get { return _pageIsLoading; }
+            set
+            {
+                _pageIsLoading = value;
+               // goButton.Content = (value ? "Stop" : "Go");
+                progressRing1.Visibility = (value ? Visibility.Visible : Visibility.Collapsed);
+
+                if (!value)
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event to indicate webview is starting a navigation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            string url = "";
+            try { url = args.Uri.ToString(); }
+            finally
+            {
+               // address.Text = url;
+                
+                pageIsLoading = true;
+            }
+        }
+
+        /// <summary>
+        /// Event is fired by webview when the content is not a webpage, such as a file download
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void webView1_UnviewableContentIdentified(WebView sender, WebViewUnviewableContentIdentifiedEventArgs args)
+        {
+            
+            // We turn around and hand the Uri to the system launcher to launch the default handler for it
+            await Windows.System.Launcher.LaunchUriAsync(args.Uri);
+            pageIsLoading = false;
+        }
+
+        /// <summary>
+        /// Event to indicate webview has resolved the uri, and that it is loading html content
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_ContentLoading(WebView sender, WebViewContentLoadingEventArgs args)
+        {
+            string url = (args.Uri != null) ? args.Uri.ToString() : "<null>";
+        }
+
+        /// <summary>
+        /// Event to indicate that the content is fully loaded in the webview. If you need to invoke script, it is best to wait for this event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
+        {
+            string url = (args.Uri != null) ? args.Uri.ToString() : "<null>";
+           
+        }
+
+        /// <summary>
+        /// Event to indicate webview has completed the navigation, either with success or failure.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void webView1_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            pageIsLoading = false;
+            if (args.IsSuccess)
+            {
+                string url = (args.Uri != null) ? args.Uri.ToString() : "<null>";
+               
+            }
+            else
+            {
+                string url = "";
+                try { url = args.Uri.ToString(); }
+                finally
+                {
+                    //address.Text = url;
+                    
+                }
+            }
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            
+            webView.Refresh();
+        }
+
+
+        private void WebView_LoadCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            
+        }
+
+    }
+}
